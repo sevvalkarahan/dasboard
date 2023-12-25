ca07b2240dfc57c534922f8274dd896808956934
Sevval Karahan <sevvalkarhn@gmail.com>
2023-12-25 12:51:47 +0300

    weather js page has been edited.

--- weather.js
+++ weather.js
@@ -48,36 +48,36 @@ let displayResult = (result) => {
     let desc = document.querySelector('.desc');
     desc.innerText = result.weather[0].description;
 
-    
-    let i_fLike=document.querySelector('.FeelsLike span i');
-    let feelsSpan=document.querySelector('.feelsSpan');
-    feelsSpan.innerText="Feels Like";
-    i_fLike.className="fa-solid fa-temperature-three-quarters";
+
+    let i_fLike = document.querySelector('.FeelsLike span i');
+    let feelsSpan = document.querySelector('.feelsSpan');
+    feelsSpan.innerText = "Feels Like";
+    i_fLike.className = "fa-solid fa-temperature-three-quarters";
     let feelLikes = document.querySelector('.rFeel');
     feelLikes.innerText = ` ${Math.round(result.main.feels_like)}°C`;
-    let i_wind=document.querySelector('.wind span i');
-    i_wind.className="fa-solid fa-wind";
-    let windSpan=document.querySelector('.windSpan');
-    windSpan.innerText="Wind";
+    let i_wind = document.querySelector('.wind span i');
+    i_wind.className = "fa-solid fa-wind";
+    let windSpan = document.querySelector('.windSpan');
+    windSpan.innerText = "Wind";
     let wind = document.querySelector('.windSpeed');
     wind.innerText = `${result.wind.speed} km/sa`;
-    let i_hum=document.querySelector('.humidity span i');
-    i_hum.className="fa-solid fa-droplet";
-    let humSpan=document.querySelector('.humSpan');
-    humSpan.innerText="Humidity";
+    let i_hum = document.querySelector('.humidity span i');
+    i_hum.className = "fa-solid fa-droplet";
+    let humSpan = document.querySelector('.humSpan');
+    humSpan.innerText = "Humidity";
     let hum = document.querySelector('.hum');
     hum.innerText = `%${result.main.humidity}`;
-    let i_sun=document.querySelector('.sunrise_sunset span i');
-    i_sun.className="fa-regular fa-sun";
-    let sunSpan=document.querySelector('.sunSpan');
-    sunSpan.innerText="Sunrise/Sunset";
+    let i_sun = document.querySelector('.sunrise_sunset span i');
+    i_sun.className = "fa-regular fa-sun";
+    let sunSpan = document.querySelector('.sunSpan');
+    sunSpan.innerText = "Sunrise/Sunset";
     let sun = document.querySelector('.sun');
     let sunrise = new Date((result.sys.sunrise * 1000));
     let sunset = new Date((result.sys.sunset * 1000))
     sun.innerText = `${sunrise.getHours()}:${sunrise.getMinutes()} / ${sunset.getHours()}:${sunset.getMinutes()}`;
 
     let image = document.querySelector('.top-left img').setAttribute("src", `https://openweathermap.org/img/wn/${result.weather[0].icon}@4x.png`);
-    let first=document.querySelector('.first');
+    let first = document.querySelector('.first');
     first.classList.add('active')
 
 
@@ -91,31 +91,28 @@ let getResponse = (cityName) => {
 let displayResponse = (result) => {
     console.log(result)
     let image = document.querySelectorAll('.bottom-container div img');
-    image.forEach((imageWeather,index)=>{
+    image.forEach((imageWeather, index) => {
         imageWeather.setAttribute("src", `https://openweathermap.org/img/wn/${result.list[index].weather[0].icon}@2x.png`);
     })
-    let wdescription=document.querySelectorAll('.description');
-    wdescription.forEach((day,index)=>{
-        day.innerText=result.list[index].weather[0].description;
+    let wdescription = document.querySelectorAll('.description');
+    wdescription.forEach((day, index) => {
+        day.innerText = result.list[index].weather[0].description;
     });
-    let dayName=document.querySelectorAll('.bottom-container div .day');
-    let days=["pazar" , "pazartesi" , "salı" , "çarşamba" , "perşembe" , "cuma" , "cumartesi"];
-
-    dayName.forEach((dName,index)=>{
-        let date=new Date();
-        dName.innerText=days[date.getDay(result.list[index])];
+    let dayName = document.querySelectorAll('.bottom-container div .day');
+    dayName.forEach((dName, index) => {
+        dName.innerText = hour((result.list[index].dt_txt), 16);
     });
-    let min_max=document.querySelectorAll('.bottom-container div .min-max-temp');
-    min_max.forEach((minMax,index)=>{
-        minMax.innerText=`${Math.round(result.list[index].main.temp_min)}°C / ${Math.round(result.list[index].main.temp_max)}°C`
+    let min_max = document.querySelectorAll('.bottom-container div .min-max-temp');
+    min_max.forEach((minMax, index) => {
+        minMax.innerText = `${Math.round(result.list[index].main.temp)}°C / ${Math.round(result.list[index].main.feels_like)}°C`
     });
-    let activeDiv=document.querySelectorAll('.bottom-container div');
-    activeDiv.forEach(active_div=>{
+    let activeDiv = document.querySelectorAll('.bottom-container div');
+    activeDiv.forEach(active_div => {
         active_div.classList.add('active')
     });
 
 
-            
+
 }
 
 
@@ -131,81 +128,83 @@ let searchBar = document.querySelector('.searchCity');
 searchBar.addEventListener('keypress', setQuery);
 
 
-function page_loading(){
-     fetch("https://api.openweathermap.org/data/2.5/weather?q=Sivas&appid=4ac125c3cead0ab707544840eae7021f&units=metric&lang=tr").then(res=>{
+function page_loading() {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=Sivas&appid=4ac125c3cead0ab707544840eae7021f&units=metric&lang=tr").then(res => {
         return res.json();
-    }).then(result=>{
+    }).then(result => {
         console.log(result)
         let city = document.querySelector('.city');
-    city.innerText = `${result.name}, ${result.sys.country}`
-    let degree = document.querySelector('.degree');
-    degree.innerText = `${Math.round(result.main.temp)}°C`
-    let desc = document.querySelector('.desc');
-    desc.innerText = result.weather[0].description;
+        city.innerText = `${result.name}, ${result.sys.country}`
+        let degree = document.querySelector('.degree');
+        degree.innerText = `${Math.round(result.main.temp)}°C`
+        let desc = document.querySelector('.desc');
+        desc.innerText = result.weather[0].description;
 
-    
-    let i_fLike=document.querySelector('.FeelsLike span i');
-    let feelsSpan=document.querySelector('.feelsSpan');
-    feelsSpan.innerText="Feels Like";
-    i_fLike.className="fa-solid fa-temperature-three-quarters";
-    let feelLikes = document.querySelector('.rFeel');
-    feelLikes.innerText = ` ${Math.round(result.main.feels_like)}°C`;
-    let i_wind=document.querySelector('.wind span i');
-    i_wind.className="fa-solid fa-wind";
-    let windSpan=document.querySelector('.windSpan');
-    windSpan.innerText="Wind";
-    let wind = document.querySelector('.windSpeed');
-    wind.innerText = `${result.wind.speed} km/sa`;
-    let i_hum=document.querySelector('.humidity span i');
-    i_hum.className="fa-solid fa-droplet";
-    let humSpan=document.querySelector('.humSpan');
-    humSpan.innerText="Humidity";
-    let hum = document.querySelector('.hum');
-    hum.innerText = `%${result.main.humidity}`;
-    let i_sun=document.querySelector('.sunrise_sunset span i');
-    i_sun.className="fa-regular fa-sun";
-    let sunSpan=document.querySelector('.sunSpan');
-    sunSpan.innerText="Sunrise/Sunset";
-    let sun = document.querySelector('.sun');
-    let sunrise = new Date((result.sys.sunrise * 1000));
-    let sunset = new Date((result.sys.sunset * 1000))
-    sun.innerText = `${sunrise.getHours()}:${sunrise.getMinutes()} / ${sunset.getHours()}:${sunset.getMinutes()}`;
 
-    let image = document.querySelector('.top-left img').setAttribute("src", `https://openweathermap.org/img/wn/${result.weather[0].icon}@4x.png`);
-    let first=document.querySelector('.first');
-    first.classList.add('active')
+        let i_fLike = document.querySelector('.FeelsLike span i');
+        let feelsSpan = document.querySelector('.feelsSpan');
+        feelsSpan.innerText = "Feels Like";
+        i_fLike.className = "fa-solid fa-temperature-three-quarters";
+        let feelLikes = document.querySelector('.rFeel');
+        feelLikes.innerText = ` ${Math.round(result.main.feels_like)}°C`;
+        let i_wind = document.querySelector('.wind span i');
+        i_wind.className = "fa-solid fa-wind";
+        let windSpan = document.querySelector('.windSpan');
+        windSpan.innerText = "Wind";
+        let wind = document.querySelector('.windSpeed');
+        wind.innerText = `${result.wind.speed} km/sa`;
+        let i_hum = document.querySelector('.humidity span i');
+        i_hum.className = "fa-solid fa-droplet";
+        let humSpan = document.querySelector('.humSpan');
+        humSpan.innerText = "Humidity";
+        let hum = document.querySelector('.hum');
+        hum.innerText = `%${result.main.humidity}`;
+        let i_sun = document.querySelector('.sunrise_sunset span i');
+        i_sun.className = "fa-regular fa-sun";
+        let sunSpan = document.querySelector('.sunSpan');
+        sunSpan.innerText = "Sunrise/Sunset";
+        let sun = document.querySelector('.sun');
+        let sunrise = new Date((result.sys.sunrise * 1000));
+        let sunset = new Date((result.sys.sunset * 1000))
+        sun.innerText = `${sunrise.getHours()}:${sunrise.getMinutes()} / ${sunset.getHours()}:${sunset.getMinutes()}`;
+
+        let image = document.querySelector('.top-left img').setAttribute("src", `https://openweathermap.org/img/wn/${result.weather[0].icon}@4x.png`);
+        let first = document.querySelector('.first');
+        first.classList.add('active')
     });
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=Sivas&appid=4ac125c3cead0ab707544840eae7021f&units=metric&lang=tr").then(res=>{
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=Sivas&appid=4ac125c3cead0ab707544840eae7021f&units=metric&lang=tr").then(res => {
         return res.json();
-    }).then(result=>{
+    }).then(result => {
         console.log(result)
         let image = document.querySelectorAll('.bottom-container div img');
-    image.forEach((imageWeather,index)=>{
-        imageWeather.setAttribute("src", `https://openweathermap.org/img/wn/${result.list[index].weather[0].icon}@2x.png`);
-    })
-    let wdescription=document.querySelectorAll('.description');
-    wdescription.forEach((day,index)=>{
-        day.innerText=result.list[index].weather[0].description;
-    });
-    let dayName=document.querySelectorAll('.bottom-container div .day');
-    let days=["pazar" , "pazartesi" , "salı" , "çarşamba" , "perşembe" , "cuma" , "cumartesi"];
+        image.forEach((imageWeather, index) => {
+            imageWeather.setAttribute("src", `https://openweathermap.org/img/wn/${result.list[index].weather[0].icon}@2x.png`);
+        })
+        let wdescription = document.querySelectorAll('.description');
+        wdescription.forEach((day, index) => {
+            day.innerText = result.list[index].weather[0].description;
+        });
+        let dayName = document.querySelectorAll('.bottom-container div .day');
+        dayName.forEach((dName, index) => {
 
-    dayName.forEach((dName,index)=>{
-        let date=new Date();
-        dName.innerText=days[date.getDay(result.list[index])];
-    });
-    let min_max=document.querySelectorAll('.bottom-container div .min-max-temp');
-    min_max.forEach((minMax,index)=>{
-        minMax.innerText=`${Math.round(result.list[index].main.temp_min)}°C / ${Math.round(result.list[index].main.temp_max)}°C`
-    });
-    let activeDiv=document.querySelectorAll('.bottom-container div');
-    activeDiv.forEach(active_div=>{
-        active_div.classList.add('active')
-    });
+            dName.innerText = hour((result.list[index].dt_txt), 16);
+        });
+        let min_max = document.querySelectorAll('.bottom-container div .min-max-temp');
+        min_max.forEach((minMax, index) => {
+            minMax.innerText = `${Math.round(result.list[index].main.temp)}°C / ${Math.round(result.list[index].main.feels_like)}°C`
+        });
+        let activeDiv = document.querySelectorAll('.bottom-container div');
+        activeDiv.forEach(active_div => {
+            active_div.classList.add('active')
+        });
     })
 
-    
+
+}
+
+function hour(txt, max) {
+    return txt = txt.substring(10, max);
 }
 
 
